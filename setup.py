6a9c9b6f6154386d42ec7f8b33eb208a17322a78
tomackl <tomackl@users.noreply.github.com>
2019-04-24 21:44:00 +1000

    20190424
    Modifications to allow Python Wheel to be produced.

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='templar',
-    version='2019.01.00',
+    version='2019.01.0',
     author='Tom Ackland',
+    author_email='ackland dot thomas at gmail dot com',
     description='A app for bashing .xlsx files into .docx.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     python_requires=">=3.7",
-    packages=find_packages(where='src'),  # since it hasn't been tested on anything else.
+    packages=find_packages(where='src'),
     package_dir={'': 'src'},
     include_package_data=True,
     install_requires=[
@@ -18,14 +24,12 @@ setup(
     entry_points={
         'console_scripts': [
             'templar = templar:cli',
-            # 'templar = templar.src.templar:cli',
         ],
     },
     classifiers=[
-        # 'Development Status :: 5 - Production/Stable',
+        'Development Status :: 4 - Beta',
         'Operating System :: MacOS :: MacOS X',
-        # 'Operating System :: Microsoft :: Windows',
-        # 'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.7',
     ],
