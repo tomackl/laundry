0ff020ff09b68ed9ba11912af412c41f461412dd
tomackl <tomackl@users.noreply.github.com>
2019-04-24 15:56:25 +1000

    20190423
    Setup.py has been modified.

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='templar',
     version='2019.01.00',
-    packages=find_packages(where='src'),
+    author='Tom Ackland',
+    description='A app for bashing .xlsx files into .docx.',
+    python_requires=">=3.7",
+    packages=find_packages(where='src'),  # since it hasn't been tested on anything else.
     package_dir={'': 'src'},
-    # py_modules=['templar'],
     include_package_data=True,
     install_requires=[
         'Click',
@@ -19,5 +21,13 @@ setup(
             # 'templar = templar.src.templar:cli',
         ],
     },
+    classifiers=[
+        # 'Development Status :: 5 - Production/Stable',
+        'Operating System :: MacOS :: MacOS X',
+        # 'Operating System :: Microsoft :: Windows',
+        # 'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
 
