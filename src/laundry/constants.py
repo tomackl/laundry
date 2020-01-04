a8b7ae9febc6a75533d3a7c23c4c38f526e316af
tomackl <tomackl@users.noreply.github.com>
2020-01-04 12:07:27 +1000

    2020.1.2 Addressed issue with photo paths
    Signed-off-by: tomackl <tomackl@users.noreply.github.com>

--- src/laundry/constants.py
+++ src/laundry/constants.py
@@ -3,9 +3,8 @@ Constants associated with Laundry/
 """
 from typing import NewType
 import pandas as pd
-import numpy as np
-laundry_version = '2020.1.2b'
+laundry_version = '2020.1.2'
 
 data_frame = NewType('data_frame', pd.DataFrame)
-invalid = ['nan', 'None', 'NA', 'N/A', 'False', 'Nil', np.nan]
+invalid = ['nan', 'None', 'NA', 'N/A', 'False', 'Nil']
 photo_formats = ['.jpg', '.jpeg', '.png', '.tiff']
